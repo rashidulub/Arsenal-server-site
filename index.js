82aca55e3446ac242b8102c852715d715f9f3f53
rashidulub <rashidul016094@gmail.com>
2023-06-10 15:52:43 +0600

    users load in ui

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ async function run() {
 
     // user related api 
 
+    app.get('/user',async(req,res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
+
+
+
     app.post('/user',async(req,res)=>{
       const user = req.body;
       const query ={email: user.email}
