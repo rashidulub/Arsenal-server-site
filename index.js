861fdd0fa10d7ddc7c2f1e1e7f765933571e66c0
rashidulub <rashidul016094@gmail.com>
2023-06-10 16:49:58 +0600

    user made instructor

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 require('dotenv').config()
 const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
