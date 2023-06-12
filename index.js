27f208f48e67700f35b88b8fb1e103cdffc82f7b
rashidulub <rashidul016094@gmail.com>
2023-06-12 18:43:04 +0600

    few error solved

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const menuCollection = client.db('summerDb').collection('menu')
     const classesCollection = client.db('summerDb').collection('classes')
